d4c4a9df3215958dbdec017eef04a1f9dab1329d
Oh-Kang94 <okh19941994@naver.com>
2023-10-20 17:47:42 +0900

    add Redis

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,17 @@
 import os 
 from flask import Flask, render_template
 from flask_cors import CORS
+from flask_session import Session
+from flask_socketio import SocketIO
 from .config.Config import api, db, jwt
-from .config.DBConfig import DBConfig
+from .config.DBConfig import MySQLConfig
 from .controller.controller import register_namespaces
 import warnings
 
 app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False
-app.config.from_object(DBConfig)
+app.config.from_object(MySQLConfig)
 # app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 30 * 24 *  60 * 60
 # app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 
 app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY')
@@ -18,6 +20,7 @@ api.init_app(app)
 db.init_app(app)
 jwt.init_app(app)
 register_namespaces(api)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # @app.route('/map/preview')
 # def previewMap():
--- app/config/DBConfig.py
+++ app/config/DBConfig.py
@@ -1,4 +1,5 @@
 import os 
+import redis
 
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
@@ -6,7 +7,19 @@ DB_PORT = os.environ.get('DB_PORT')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_DATABASE = os.environ.get('DB_DATABASE')
 
-class DBConfig:
+## REDIS 설정
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = os.environ.get("REDIS_PORT")
+
+class MySQLConfig:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Config(object):
+    # Parse redis environment variables.
+    SESSION_TYPE = "redis"
+    redis_client = redis.Redis(
+        host=REDIS_HOST, port=REDIS_PORT
+    )
+    SESSION_REDIS = redis_client
